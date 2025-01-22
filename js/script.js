24af04062ba9813b1f985bdd9aa616a4077409e9
Giovanni <dibellogio@gmail.com>
2025-01-22 16:09:39 +0100

    fix add array email

--- js/script.js
+++ js/script.js
@@ -3,36 +3,41 @@ const listaOutput = document.getElementById("listaEmail");
 const button = document.getElementById("button");
 
 // invoco la funzione
-creaEmailRandom ()
+creaEmailRandom()
+
+const emailRandom = [];
 
 // creo una funzione che genera le 10 email random
-function creaEmailRandom () {
+function creaEmailRandom() {    
 
-// creo un ciclo for che genera 10 email random
-for (let i=0; i < 10; i++) {
+    // creo un ciclo for che genera 10 email random
+    for (let i = 0; i < 10; i++) {
 
-// genero la mail random tramite l'api
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-.then(risposta =>{
 
-    // prendo il dato della email
-    const emailRandom = risposta.data.response;
-    console.log(emailRandom);
+        // genero la mail random tramite l'api
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(risposta => {
 
-    // stampo in pagina le mail
-    listaOutput.innerHTML += `<li>${emailRandom}</li>`   
+                // prendo il dato della email
+                emailRandom.push(risposta.data.response);
+                console.log(risposta.data.response);
 
-})
-.catch(error => {
-    console.error(error);    
-})
+                // stampo in pagina le mail
+                listaOutput.innerHTML += `<li>${risposta.data.response}</li>            `
+             
 
-}
+            })
+            .catch(error => {
+                console.error(error);
+            })
+
+    }
 
 }
 
 // creo al click la simulazione del fetch
 button.addEventListener("click", function () {
+    emailRandom.length = 0;
     listaOutput.innerHTML = "";
-    return creaEmailRandom ()
+    return creaEmailRandom()
 })
